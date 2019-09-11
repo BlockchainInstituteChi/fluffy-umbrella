5976922f5f861593570263fd5e46495a77cd3d44
alexander-morris <alex@theblockchaininstitute.org>
2019-09-11 12:55:13 -0500

    building for static deployment

---
+++ docs/precache-manifest.8f52b668317b2924b4436762ae921cd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82566f8654f76403a9ef43eb62903575",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bcd310f8ab7e4404a2e",
+    "url": "/static/css/main.554debca.chunk.css"
+  },
+  {
+    "revision": "319553d780cbb0eba920",
+    "url": "/static/js/2.122bc195.chunk.js"
+  },
+  {
+    "revision": "3bcd310f8ab7e4404a2e",
+    "url": "/static/js/main.7533d492.chunk.js"
+  },
+  {
+    "revision": "0bcf38b1c1ecaefeaf14",
+    "url": "/static/js/runtime~main.db2587c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
