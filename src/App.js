13e942b7230c7d328ab098aa89fa89ffd73558ab
alexander-morris <alex@theblockchaininstitute.org>
2019-09-11 13:45:41 -0500

    Adding mode switch by get param '?mode='

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import sha256 from 'sha256';
+import { validateMnemonic, mnemonicToSeedSync, generateMnemonic, mnemonicToEntropy } from 'bip39';
+import qs from 'query-string';
+
+const bip39 = { validateMnemonic, mnemonicToSeedSync, generateMnemonic, mnemonicToEntropy };
 
 class App extends React.Component {
 
@@ -43,14 +47,36 @@ class App extends React.Component {
 
   componentDidMount ( props ) { 
     this.textInput.focus();
+
     console.log('component mounted!')
+
+    setExampleMode(this);
+
+    function setExampleMode (_this) {
+
+      var mode = qs.parse(window.location.search, { ignoreQueryPrefix: true }).mode;
+      console.log('mode', mode)
+      if ( typeof(mode) === 'undefined' ) {
+        _this.setState({ mode: 'sha256' }) 
+      } else if ( mode === "bip39" ) {
+        _this.setState({ mode: 'bip39' })
+      } else {
+        _this.setState({ mode: 'sha256' }) 
+      }
+
+    }
 
   }
 
   handleChange () {
 
-    var text = this.textInput.value,
-        output = sha256(text);
+    var text = this.textInput.value;
+
+    if (this.state.mode === 'bip39') {
+      var output = bip39.generateMnemonic();
+    } else {
+      var output = sha256(text);
+    }
 
     this.setState({
       input: text, 
